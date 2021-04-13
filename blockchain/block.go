fbdd8c5ae13fd1cd9e6c2d8bf89db1c1b0a029e1
Jose Henrique Felipetto <jose.felipetto@gmail.com>
2021-04-13 10:14:05 -0300

    Finished implementing badger db and cli

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,8 +1,10 @@
 package blockchain
 
-type BlockChain struct {
-	Blocks []*Block
-}
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 type Block struct {
 	Hash []byte
@@ -21,18 +23,34 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-
-
-func (c *BlockChain) AddBlock(data string)  {
-	prevBlock := c.Blocks[len(c.Blocks) - 1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	c.Blocks = append(c.Blocks, newBlock)
-}
-
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain()  *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+func (b * Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	Handle(err)
+
+	return res.Bytes()
+}
+
+func Deserialize(serialized []byte) * Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(serialized))
+
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error)  {
+	if err != nil {
+		log.Panic(err)
+	}
 }
