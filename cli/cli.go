0505f6e1cffe94349d3bd33c23df59eb3936c9bc
Jose Henrique Felipetto <jose.felipetto@gmail.com>
2021-04-18 14:32:32 -0300

    Added basic wallet module

--- cli/cli.go
+++ cli/cli.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/josefelipetto/golang-blockchain/blockchain"
+	"github.com/josefelipetto/golang-blockchain/wallet"
 	"log"
 	"os"
 	"runtime"
@@ -11,43 +12,6 @@ import (
 )
 
 type CommandLine struct{}
-
-func (cli *CommandLine) printUsage() {
-	fmt.Println("Usage:")
-	fmt.Println(" print - Prints the blocks in the chain")
-	fmt.Println(" getbalance -address ADDRESS - get balance for address")
-	fmt.Println(" createblockchain -address ADDRESS creates a blockchain")
-	fmt.Println(" send -from FROM -to TO -amount AMOUNT - Send an amount from FROM to TO")
-
-}
-
-func (cli *CommandLine) validateArgs() {
-	if len(os.Args) < 2 {
-		cli.printUsage()
-		runtime.Goexit()
-	}
-}
-
-func (cli *CommandLine) printChain() {
-	chain := blockchain.ContinueBlockChain("")
-	defer chain.Database.Close()
-	iter := chain.Iterator()
-
-	for {
-		block := iter.Next()
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println("=====================================")
-
-		pow := blockchain.NewProof(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-
-		if len(block.PrevHash) == 0 {
-			break
-		}
-	}
-}
 
 func (cli *CommandLine) Run() {
 	cli.validateArgs()
@@ -62,6 +26,9 @@ func (cli *CommandLine) Run() {
 	sendFrom := sendCmd.String("from", "", "Source wallet address")
 	sendTo := sendCmd.String("to", "", "Destination wallet address")
 	sendAmount := sendCmd.Int("amount", 0, "Amount to send")
+
+	createWalletCmd := flag.NewFlagSet("createwallet", flag.ExitOnError)
+	listAddressesCmd := flag.NewFlagSet("listaddresses", flag.ExitOnError)
 
 	switch os.Args[1] {
 	case "print":
@@ -82,7 +49,16 @@ func (cli *CommandLine) Run() {
 		if err != nil {
 			log.Panic(err)
 		}
-
+	case "createwallet":
+		err := createWalletCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "listaddresses":
+		err := listAddressesCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
 	default:
 		cli.printUsage()
 		runtime.Goexit()
@@ -115,6 +91,52 @@ func (cli *CommandLine) Run() {
 		}
 		cli.send(*sendFrom, *sendTo, *sendAmount)
 	}
+
+	if createWalletCmd.Parsed() {
+		cli.createWallet()
+	}
+
+	if listAddressesCmd.Parsed() {
+		cli.listAddresses()
+	}
+}
+
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println(" print - Prints the blocks in the chain")
+	fmt.Println(" getbalance -address ADDRESS - get balance for address")
+	fmt.Println(" createblockchain -address ADDRESS creates a blockchain")
+	fmt.Println(" send -from FROM -to TO -amount AMOUNT - Send an amount from FROM to TO")
+	fmt.Println(" createwallet - Creates a New Wallet")
+	fmt.Println(" listaddress - List the addresses in our wallet file")
+}
+
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+func (cli *CommandLine) printChain() {
+	chain := blockchain.ContinueBlockChain("")
+	defer chain.Database.Close()
+	iter := chain.Iterator()
+
+	for {
+		block := iter.Next()
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println("=====================================")
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
 }
 
 func (cli *CommandLine) createBlockChain(address string) {
@@ -144,4 +166,21 @@ func (cli *CommandLine) send(from, to string, amount int) {
 	tx := blockchain.NewTransaction(from, to, amount, chain)
 	chain.AddBlock([]*blockchain.Transaction{tx})
 	fmt.Println("Sent successfully")
+}
+
+func (cli *CommandLine) listAddresses() {
+	wallets, _ := wallet.CreateWallets()
+	addresses := wallets.GetAllAddresses()
+
+	for _, address := range addresses {
+		fmt.Println(address)
+	}
+}
+
+func (cli * CommandLine) createWallet()  {
+	wallets, _ := wallet.CreateWallets()
+	address := wallets.AddWallet()
+	wallets.SaveFile()
+
+	fmt.Printf("New address is: %s\n", address)
 }
